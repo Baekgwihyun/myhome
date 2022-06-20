266e8997146bb7da332ea0eabe96c90c187d0bf1
qorrnlgus95 <qorrnlgus95@maver.com>
2022-06-20 16:22:32 +0900

    init

---
+++ src/main/java/com/example/demo/validator/BoardValidator.java
@@ -0,0 +1,27 @@
+package com.example.demo.validator;
+
+
+import com.example.demo.model.Board;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.thymeleaf.util.StringUtils;
+
+
+@Component
+public class BoardValidator implements Validator { // 스프링 부트 유효성 검사
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Board.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object obj, Errors errors) {
+        Board b = (Board) obj;
+        if(StringUtils.isEmpty(b.getContent())){
+            errors.rejectValue("content", "key", "내용을 입력하세요");
+        }
+
+
+    }
+}
